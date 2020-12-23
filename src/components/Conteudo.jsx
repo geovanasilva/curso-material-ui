d6c0031c4498ea56890c92f1a02c582d245d7f80
Geovana Silva <sts.geovana@gmail.com>
2020-12-23 00:59:23 -0300

    feat: estilizando componentes com makeStyles

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Formulario from './Formulario';
+
+const useStyles = makeStyles({
+    botaoFinalizarPagamento: {
+        borderRadius: 0,
+        boxShadow: "10px 10px 30px rgba(255, 203,71, 0.5)",
+        color: "white",
+        fontSize: 12,
+        fontWeight: 700,
+        height: 50,
+        margin: "20px 20px 100px 0px",
+        textTransform: "none",
+        width: 200,
+    },
+});
 
 export default function Conteudo() {
+    const classes = useStyles();
     return (
         <div>
-            <Button>Finalizar pagamento</Button>
+            <Formulario/>
+            <Button variant="contained" color="primary" className={classes.botaoFinalizarPagamento}>Finalizar pagamento</Button>
         </div>
     )
 }
